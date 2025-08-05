d7ec87554d42da695f31bfccdafdac8fa373b9b8
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-05 20:23:10 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/whitecloud/main.go
@@ -0,0 +1,23 @@
+// cmd/whitecloud/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"whitecloud/internal/whitecloud"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := whitecloud.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
